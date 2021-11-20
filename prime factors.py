2777605725d9ec6ea3f627892564b0ab302112ca
Jai Verma <94690866+Jai-Verma-04@users.noreply.github.com>
2021-11-20 12:47:16 +0530

    Add files via upload

---
+++ prime factors.py
@@ -0,0 +1,46 @@
+'''rime Factorization - Have the user enter a number and \
+find all Prime Factors (if there are any) and display them.'''
+
+num = int(input("Enter the number to find prime factors for: "))
+factors = [1]
+factors_sort = []
+final = []
+
+########################################################
+def check_prime(num):
+    flag = False
+    if num >= 1:
+        for i in range(2, int(num/2)+1):
+            if (num % i) == 0:
+                flag = False
+                break
+        else:
+            flag =True
+    else:
+        flag = False
+    
+    return flag
+
+########################################################
+if num>=1:
+    for i in range(2, int(num/2)+1):
+        if (num % i) == 0:
+            factors_sort.append(i)
+    else:
+        factors_sort.append(None)
+
+########################################################
+for k in factors_sort:
+    if k != None:
+        factors.append(k)
+
+########################################################
+if len(factors)!=0: 
+    for j in factors:
+        if check_prime(j)==True:
+            final.append(j)
+    print("Prime Factors are: ",final)           
+    if final == [1]:
+        print("Number given was already prime.")
+
+########################################################
